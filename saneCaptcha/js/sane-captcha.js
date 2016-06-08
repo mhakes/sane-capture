2f734a611f15eee823379d325362b7cb9367297a
Matt Hakes <vficto@gmail.com>
2016-06-08 16:22:35 -0500

    Update sane-captcha.js

--- saneCaptcha/js/sane-captcha.js
+++ saneCaptcha/js/sane-captcha.js
@@ -159,19 +159,3 @@ const saneCapture = (args = null) => {
     }
     return sendBack;
 };
-let validator = saneCapture({
-    success: () => {
-        alert("Very Good Choice");
-    },
-    failure: () => {
-        $('#directionsCol').empty().append('<p class="lead text-xs-center">Run Away!</p>');
-        _.delay(function() {
-            //  window.location = 'http://wikipedia.com';
-            console.log('I am outta here!');
-        }, 3000);
-    },
-    appendTo: $('#putHere')
-}).create({
-    numIcons: 65,
-    additionalClasses: ['vert-pos', 'toWhite']
-});
