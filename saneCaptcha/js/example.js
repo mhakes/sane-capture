c831e0745ce6b6cdb14385da6e28fe6c28a61909
Matt Hakes <vficto@gmail.com>
2016-07-01 16:25:09 -0500

    Update example.js

--- saneCaptcha/js/example.js
+++ saneCaptcha/js/example.js
@@ -1,18 +1,31 @@
 "use strict";
+let d = 'Just to prove that your a human being and not a robot:<br>';
+d += 'Please click on each of the green icons below that match the 3 blue icons above';
+let k = saneCaptcha({
+    numberOfIcons: 30,
+    pathToIcons: 'fa.json',
+    instructions: d,
 
-let validator = saneCapture({
     success: () => {
-        alert("Very Good Choice");
+        $('#saneCaptchaCol').slideUp('fast', function() {
+            $(this).empty().append('<p id="goodJob" class="text-xs-center lead"></p>');
+            $('#goodJob').text('Yep, you have some human DNA!');
+            $(this).slideDown();
+        });
     },
     failure: () => {
-        $('#directionsCol').empty().append('<p class="lead text-xs-center">Run Away!</p>');
-        _.delay(function() {
-            //  window.location = 'http://wikipedia.com';
-            console.log('I am outta here!');
-        }, 3000);
-    },
-    appendTo: $('#putHere')
-}).create({
-    numIcons: 65,
-    additionalClasses: ['vert-pos', 'toWhite']
+        $('#saneCaptchaCol').empty().append('<p id="badJob" class="text-xs-center lead"></p>');
+        $('#badJob').html("Sorry, you don\'t appear to be human!<br/>Additional Help Available ");
+
+        $('<a/>',{
+            href:'http://electricpulp.com/guykawasaki/arse/',
+            'class': 'btn btn-danger',
+            text: 'Here'
+        }).appendTo('#badJob');
+        $(this).slideDown();
+    }
 });
+k.setup({
+    appendTo: $('#putHere')
+});
+k.create();
